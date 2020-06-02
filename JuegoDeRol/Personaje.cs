38cbe2f0f5760a41dda5edc6a22cc56e54ecb2e3
Guido D'Arpino <gldarpino@gmail.com>
2020-06-02 16:02:39 -0300

    Generacion del personaje

---
+++ JuegoDeRol/Personaje.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace JuegoDeRol
+{
+
+    public class Personaje
+    {
+        public Personaje()
+        {
+            int Velocidad;
+            int Destreza;
+            int Fuerza;
+            int Nivel;
+            int Armadura;
+
+            int Tipo;
+            string Nombre;
+            string Apodo;
+            DateTime FechaDeNacimiento;
+            int Edad; //entre 0 a 300
+            int Salud;//100
+
+
+        }
+    }
+    enum Maximos
+    {
+        Velocidad =10,
+        Destreza =5,
+        Fuerza =10,
+        Nivel =10,
+        Armadura=10
+    }
+}
